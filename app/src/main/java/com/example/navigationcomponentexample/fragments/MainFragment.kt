72ec4e215eba6652d3705003e58ccfe09c05241f
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-03 02:57:25 -0700

    Navigation Component Example

--- app/src/main/java/com/example/navigationcomponentexample/fragments/MainFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/fragments/MainFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponentexample.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +22,20 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.btn_viewTransaction).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btn_sendMoney).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btn_viewBalance).setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+
     }
 
 }
